aae146be773a1703fe325822654d7ccf696afc64
aquastias <alexandrumlakar@gmail.com>
2024-10-25 00:10:28 +0300

    Simply extensions for VSCode

--- hosts/_common/optional/programs/vscode/extensions.nix
+++ hosts/_common/optional/programs/vscode/extensions.nix
@@ -1,34 +1,29 @@
 {pkgs, ...}: {
-  programs.vscode.extensions = let
-    vscodeExtensions = pkgs.vscode-extensions;
-  in
-    builtins.filter (ext: ext != null) (
-      builtins.attrValues {
-        alejandra = vscodeExtensions.kamadorueda.alejandra;
-        catppuccinTheme = vscodeExtensions.catppuccin.catppuccin-vsc;
-        catppuccinThemeIcons = vscodeExtensions.catppuccin.catppuccin-vsc-icons;
-        eslint = vscodeExtensions.dbaeumer.vscode-eslint;
-        formatFiles = vscodeExtensions.jbockle.jbockle-format-files;
-        go = vscodeExtensions.golang.go;
-        gitGraph = vscodeExtensions.mhutchie.git-graph;
-        gitHistory = vscodeExtensions.donjayamanne.githistory;
-        gitLens = vscodeExtensions.eamodio.gitlens;
-        highlightMatchingTag = vscodeExtensions.vincaslt.highlight-matching-tag;
-        importCost = vscodeExtensions.wix.vscode-import-cost;
-        lua = vscodeExtensions.sumneko.lua;
-        markdownAllInOne = vscodeExtensions.yzhang.markdown-all-in-one;
-        nixIde = vscodeExtensions.jnoortheen.nix-ide;
-        pdfViewer = vscodeExtensions.tomoki1207.pdf;
-        prettier = vscodeExtensions.esbenp.prettier-vscode;
-        projectManager = vscodeExtensions.alefragnani.project-manager;
-        rust = vscodeExtensions.rust-lang.rust-analyzer;
-        shellCheck = vscodeExtensions.timonwong.shellcheck;
-        spellChecker = vscodeExtensions.streetsidesoftware.code-spell-checker;
-        styleLint = vscodeExtensions.stylelint.vscode-stylelint;
-        svg = vscodeExtensions.jock.svg;
-        vim = vscodeExtensions.vscodevim.vim;
-        yaml = vscodeExtensions.redhat.vscode-yaml;
-        zig = vscodeExtensions.ziglang.vscode-zig;
-      }
-    );
+  programs.vscode.extensions = with pkgs.vscode-extensions; [
+    kamadorueda.alejandra
+    catppuccin.catppuccin-vsc
+    catppuccin.catppuccin-vsc-icons
+    dbaeumer.vscode-eslint
+    jbockle.jbockle-format-files
+    golang.go
+    mhutchie.git-graph
+    donjayamanne.githistory
+    eamodio.gitlens
+    vincaslt.highlight-matching-tag
+    wix.vscode-import-cost
+    sumneko.lua
+    yzhang.markdown-all-in-one
+    jnoortheen.nix-ide
+    tomoki1207.pdf
+    esbenp.prettier-vscode
+    alefragnani.project-manager
+    rust-lang.rust-analyzer
+    timonwong.shellcheck
+    streetsidesoftware.code-spell-checker
+    stylelint.vscode-stylelint
+    jock.svg
+    vscodevim.vim
+    redhat.vscode-yaml
+    ziglang.vscode-zig
+  ];
 }
