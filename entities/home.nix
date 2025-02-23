3d256638f6f31b07bce7dbce7513f363c22ab295
aquastias <alexandrumlakar@gmail.com>
2025-02-23 16:50:24 +0200

    Fix home impermanence error

--- entities/home.nix
+++ entities/home.nix
@@ -3,19 +3,6 @@
   inputs,
   ...
 }: {
-  home = {
-    enableNixpkgsReleaseCheck = false;
-
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    stateVersion = "${configVars.version}";
-  };
-
   imports = [
     # Impermanence module
     inputs.impermanence.nixosModules.home-manager.impermanence
@@ -28,31 +15,43 @@
     "${configVars.hosts.common.optional.programs.path}/vscode"
   ];
 
-  persistence."${configVars.persistFolder}/home" = {
-    directories = [
-      "Desktop"
-      "Documents"
-      "Downloads"
-      "Music"
-      "Pictures"
-      "Public"
-      "Templates"
-      "Videos"
-      ".gnupg"
-      ".ssh"
-      ".nixops"
-      ".mozilla"
-      ".local/share/keyrings"
-      ".local/share/direnv"
-      {
-        directory = ".local/share/Steam";
-        method = "symlink";
-      }
-    ];
-    files = [
-      ".screenrc"
-    ];
-    allowOther = true;
+  home = {
+    enableNixpkgsReleaseCheck = false;
+    persistence."${configVars.persistFolder}/home" = {
+      directories = [
+        "Desktop"
+        "Documents"
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Public"
+        "Templates"
+        "Videos"
+        ".gnupg"
+        ".ssh"
+        ".nixops"
+        ".mozilla"
+        ".local/share/keyrings"
+        ".local/share/direnv"
+        {
+          directory = ".local/share/Steam";
+          method = "symlink";
+        }
+      ];
+      files = [
+        ".screenrc"
+      ];
+      allowOther = true;
+    };
+
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "${configVars.version}";
   };
 
   programs = {
