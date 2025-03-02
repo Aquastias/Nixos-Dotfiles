0ae454f595a1abdb83496373d942c7af1f0ae8bb
aquastias <alexandrumlakar@gmail.com>
2025-03-02 17:18:13 +0200

    Add chronos and eternioxs configs in hosts

---
+++ hosts/chronos/scripts/create-home-for-users.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+if ! command -v nix-instantiate &>/dev/null; then
+  echo "nix-instantiate could not be found. Please install nix-instantiate."
+  exit 127
+fi
+
+if ! command -v sed &>/dev/null; then
+  echo "sed could not be found. Please install sed."
+  exit 127
+fi
+
+users_nix_file="../users.nix"
+
+if ! users_json=$(nix-instantiate --eval --strict --json "$users_nix_file"); then
+  echo "Error evaluating users.nix"
+  exit 1
+fi
+
+users_array=$(echo "$users_json" | sed 's/[][]//g; s/"//g; s/,/\n/g')
+
+if [[ -z "$users_array" ]]; then
+  echo "Error parsing JSON (likely empty array)"
+  exit 1
+fi
+
+readarray -t users <<< "$users_array"
+
+if ! mkdir -p /mnt/persist/home; then
+  echo "Failed to create directory /mnt/persist/home"
+  exit 1
+fi
+
+for user in "${users[@]}"; do
+  if ! mkdir -p "/mnt/persist/home/$user"; then
+    echo "Failed to create directory /mnt/persist/home/$user"
+    exit 1
+  fi
+
+  echo "Created directory /mnt/persist/home/$user"
+
+  chown -R "$user" "/mnt/persist/home/$user" 
+done
---
+++ hosts/eterniox/scripts/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root (sudo)."
+  exit 1
+fi
+
+chmod +x ./run-disko.sh
+#chmod +x ./create-home-for-users.sh
+
+./run-disko.sh
+./create-home-for-users.sh
+
+rm -rf /mnt/persist/nixos
+git clone https://github.com/Aquastias/Nixos-Dotfiles.git /mnt/persist/nixos
+nixos-install --root /mnt --flake /mnt/persist/nixos#vanguard
