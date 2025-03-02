8f564e234e6f0ca2951a89626df243e6bd4372f5
aquastias <alexandrumlakar@gmail.com>
2025-03-02 15:36:28 +0200

    Update disko config

--- hosts/vanguard/disko-config.nix
+++ hosts/vanguard/disko-config.nix
@@ -6,7 +6,12 @@
         type = "disk";
         content = {
           type = "gpt";
+
           partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
             ESP = {
               size = "1G";
               type = "EF00";
@@ -38,38 +43,31 @@
     zpool = {
       zroot = {
         type = "zpool";
+
         rootFsOptions = {
+          # Source:
           # https://wiki.archlinux.org/title/Install_Arch_Linux_on_ZFS
+          # https://www.high-availability.com/docs/ZFS-Tuning-Guide/#general-recommendations
           acltype = "posixacl";
           atime = "off";
           compression = "zstd";
           mountpoint = "none";
           xattr = "sa";
         };
-        options.ashift = "12";
+
+        options = {
+          # Source:
+          # https://wiki.archlinux.org/title/Advanced_Format
+          # https://www.high-availability.com/docs/ZFS-Tuning-Guide/#alignment-shift-ashiftn
+          ashift = "12";
+        };
 
         datasets = {
-          "local" = {
+          "system" = {
             type = "zfs_fs";
             options.mountpoint = "none";
           };
-          "local/home" = {
-            type = "zfs_fs";
-            mountpoint = "/home";
-            # Used by services.zfs.autoSnapshot options.
-            options."com.sun:auto-snapshot" = "true";
-          };
-          "local/nix" = {
-            type = "zfs_fs";
-            mountpoint = "/nix";
-            options."com.sun:auto-snapshot" = "false";
-          };
-          "local/persist" = {
-            type = "zfs_fs";
-            mountpoint = "/persist";
-            options."com.sun:auto-snapshot" = "false";
-          };
-          "local/root" = {
+          "system/root" = {
             type = "zfs_fs";
             mountpoint = "/";
             options = {
@@ -79,6 +77,22 @@
               keylocation = "prompt";
             };
             postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot/local/root@blank$' || zfs snapshot zroot/local/root@blank";
+          };
+          "system/home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            # Used by services.zfs.autoSnapshot options.
+            options."com.sun:auto-snapshot" = "true";
+          };
+          "system/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "system/persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+            options."com.sun:auto-snapshot" = "false";
           };
         };
       };
