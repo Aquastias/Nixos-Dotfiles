41e24cfd09cf7975f1921bbfce10c960489a8615
aquastias <alexandrumlakar@gmail.com>
2025-03-02 20:00:09 +0200

    Add spark user

---
+++ entities/users/spark/default.nix
@@ -0,0 +1,35 @@
+{configVars, ...}: let
+  userName = "spark";
+  userEmail = "alexandrumlakar@gmail.com";
+in {
+  users.users."${userName}" = {
+    initialPassword = "password";
+    isNormalUser = true;
+    description = "Spark";
+    extraGroups = [
+      "audio"
+      "gpg" # For GnuPG
+      "scanner" # To be able to see scanner devices
+      "wheel" # Enable ‘sudo’ for the user.
+    ];
+  };
+
+  home-manager = {
+    users."${userName}" = {
+      imports = [
+        configVars.entities.home.path
+      ];
+
+      home = {
+        username = userName;
+        homeDirectory = "/home/${userName}";
+      };
+
+      programs = {
+        git = {
+          inherit userEmail userName;
+        };
+      };
+    };
+  };
+}
