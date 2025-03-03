f61be18b19ad84fbfa851ba64a2f237290d2d023
aquastias <alexandrumlakar@gmail.com>
2025-03-03 19:22:33 +0200

    Update disko config for vanguard

--- hosts/vanguard/disko-config.nix
+++ hosts/vanguard/disko-config.nix
@@ -15,7 +15,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = ["umask=0022"]; #0077 for very strict
+                mountOptions = ["umask=0077"]; #0077 for very strict
               };
             };
             swap = {
