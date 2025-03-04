821fe82b07425f4fde3443b8afa9c0693e9a72b5
aquastias <alexandrumlakar@gmail.com>
2025-03-05 01:33:08 +0200

    Update nixosConfigurations

--- flake.nix
+++ flake.nix
@@ -99,8 +99,8 @@
     # Invoke mkHost for each host config that is declared
     mkHostConfigs = hosts: lib.foldl (acc: set: acc // set) {} (lib.map (host: mkHost host) hosts);
     # Return the hosts declared in the given directory
-    readHosts = folder: lib.attrNames (builtins.readDir ./hosts/${folder});
+    readHosts = folder: lib.attrNames (builtins.readDir ./${folder});
   in {
-    nixosConfigurations = mkHostConfigs (readHosts "nixos");
+    nixosConfigurations = mkHostConfigs (readHosts "hosts");
   };
 }
