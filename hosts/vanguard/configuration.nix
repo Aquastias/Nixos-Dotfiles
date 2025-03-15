76a61956c206e7a866dbdedb72ada14a6807077a
aquastias <alexandrumlakar@gmail.com>
2025-03-15 14:09:19 +0200

    Comment authorized keys

--- hosts/vanguard/configuration.nix
+++ hosts/vanguard/configuration.nix
@@ -20,15 +20,15 @@ in {
     ]
     ++ builtins.map (user: "${entities.users.path}/${user}") users;
 
-  users.users = {
-    openssh = {
-      authorizedKeys = {
-        keys = [
-          (builtins.readFile ../../entities/users/aquastias/keys/id_aquastias.pub)
-        ];
-      };
-    };
-  };
+  # users.users = {
+  #   openssh = {
+  #     authorizedKeys = {
+  #       keys = [
+  #         (builtins.readFile ../../entities/users/aquastias/keys/id_aquastias.pub)
+  #       ];
+  #     };
+  #   };
+  # };
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
