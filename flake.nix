ba1c9c9d3191930b1458cff69f1e0ec0aff6ba27
aquastias <alexandrumlakar@gmail.com>
2025-04-14 00:21:57 +0300

    Update my-secrets url

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
     };
 
     my-secrets = {
-      url = "git+ssh://git@github.com/Aquastias/Nixos-Secrets.git";
+      url = "git+ssh://git@github.com/Aquastias/Nixos-Secrets.git?ref=main&shallow=1";
       flake = false;
     };
   };
