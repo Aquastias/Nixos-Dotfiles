1908c51976f041c277cbea63b6de7ef6ba37642e
aquastias <alexandrumlakar@gmail.com>
2025-05-19 01:21:54 +0300

    Update clone-ssh script

--- hosts/vanguard/scripts/clone-ssh.sh
+++ hosts/vanguard/scripts/clone-ssh.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Cloning SSh credential in /root.ssh ..."
+
+[ -d "/root/.ssh" ] || mkdir -p /root/.ssh
+
 git clone https://github.com/Aquastias/Vault.git --depth=1 vault
 nix-shell -p age --run "age -d -o ./vault/Git-SSH.tar.gz.enc ./vault/Git-SSH.tar.gz.enc.age"
 nix-shell -p openssl --run "openssl enc -aes-256-cbc -d -pbkdf2 -iter 100000 -salt -in ./vault/Git-SSH.tar.gz.enc | tar xzf - -C /root/.ssh"
